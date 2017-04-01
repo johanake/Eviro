fc360b67b1b0ab9d8e2083138e96324f8787c999
Peter Sjögren <peter.sjogren@live.se>
2017-04-02 00:55:54 +0200

    Revert "Kient-Server-DB-demo"
    This reverts commit 7e233a8d38ccc5e8f923b3faea89f45161086604.

---
+++ src/klientServerDbDemo/User.java
@@ -0,0 +1,19 @@
+package klientServerDbDemo;
+
+public class User {
+
+	private String mName;
+	
+	public User(String name) {
+		mName = name;
+	}
+
+	public String getmName() {
+		return mName;
+	}
+
+	public void setmName(String mName) {
+		this.mName = mName;
+	}
+
+}
