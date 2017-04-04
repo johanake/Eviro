b1de54c45f2b8769df229ecc6730d12167f8b1e0
Robin Overgaard <git@robinovergaard.com>
2017-04-04 18:47:00 +0200

    Preppat

--- src/Client.java
+++ src/Client.java
@@ -1,68 +1,12 @@
-import java.awt.Dimension;
 
-import javax.swing.JDesktopPane;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UIManager.LookAndFeelInfo;
+// Vi tömde denna klassen så det är bara att kötta Matteboi! // R & J 
 
-/**
- * Handles all major client side logical operations of the system.
- * 
- * @author Robin Overgaard
- * @version 0.1
- */
 public class Client {
 
-	/**
-	 * Constructs the client, instantiates a new main workspace window. 
-	 */
 	public Client() {
-		
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				setSystemLookAndFeel();
-				JFrame window = new JFrame(Main.APP_NAME + " " + Main.APP_VERSION);
-				window.setExtendedState(JFrame.MAXIMIZED_BOTH);
-				window.setContentPane(new JDesktopPane());
-				window.setMinimumSize(new Dimension(800, 600));
-				window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				window.setVisible(true);
-				window.setJMenuBar(new Menu()); // Example, might not be instantiated like this. 
-				window.add(new Toolbox("Tool 1", new Tool())); // Example, will not be instantiated like this. 
-				window.add(new Toolbox("Tool 2", new Demotool())); // Example, will not be instantiated like this. 
-				window.add(new Toolbox("Tool 3", new Tool())); // Example, will not be instantiated like this. 
-			}
-		});
-
+		new ClientController();
 	}
-
-	/**
-	 * 	Sets the overall look and field for the system.
-	 */
-	private void setSystemLookAndFeel() {
-
-		try {
-			
-			for (LookAndFeelInfo lnfi : UIManager.getInstalledLookAndFeels()) {
-
-				if ("Nimbus".equals(lnfi.getName())) {
-					UIManager.setLookAndFeel(lnfi.getClassName());
-					break;
-				}
-				
-			}
-
-		} catch (Exception e) {
-			
-			try {
-				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
-			
-		}
-		
-	}
+	
+	//..
 	
 }
