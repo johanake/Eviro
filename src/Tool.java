b82f7d6a487d7115fe3b285877b5bf1cd7958fa9
Robin Overgaard <git@robinovergaard.com>
2017-04-05 23:20:09 +0200

    Comments on tool.java

--- src/Tool.java
+++ src/Tool.java
@@ -1,6 +1,18 @@
+/**
+ * Interface for Tools in the system.
+ * 
+ * @author Robin Overgaard
+ * @version 1.0
+ */
+
 public interface Tool {
+
+	/**
+	 * Returns the name of the tool
+	 * @return the name of the tool
+	 */
+	public String getTitle();
 	
-	   public String getTitle();
-//	   public String sendDataToClient();
-	   
+	// public String sendDataToClient();
+
 }
