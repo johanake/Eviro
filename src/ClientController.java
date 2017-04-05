046465171634e0930d091d510c0c44f1da5e5422
Robin Overgaard <git@robinovergaard.com>
2017-04-05 14:35:25 +0200

    Added ActionJButton class.

--- src/ClientController.java
+++ src/ClientController.java
@@ -1,10 +1,12 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JDesktopPane;
@@ -20,52 +22,47 @@ import javax.swing.border.TitledBorder;
 
 /**
  * Handles all major client side logical operations of the system.
- * 
  * @author Robin Overgaard
  * @version 0.1
  */
 
 public class ClientController {
+	
 	private JDesktopPane desktop;
 
 	/**
 	 * Constructs the client, instantiates a new main workspace window.
-	 * @return 
 	 */
 	public ClientController() {
-
+		
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				setSystemLookAndFeel();
 				JFrame window = new JFrame(Main.APP_NAME + " " + Main.APP_VERSION);
 				JPanel pnlMain = new JPanel(new BorderLayout());
 				desktop = new JDesktopPane();
-				pnlMain.add(desktop, BorderLayout.CENTER);
-				pnlMain.add(new Sidebar(), BorderLayout.WEST);
+				
 				window.setContentPane(pnlMain);
+				window.add(desktop, BorderLayout.CENTER);
+				window.add(new Sidebar(), BorderLayout.WEST);
 				window.setExtendedState(JFrame.MAXIMIZED_BOTH);
 				window.setMinimumSize(new Dimension(800, 600));
 				window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				window.setVisible(true);
 				window.setJMenuBar(new Menu()); 
-
+				window.setIconImage(new ImageIcon(Main.APP_ICON).getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH));
 			}
 		});
 
 	}
-	
-	// Lägga desktop som instansvariabel så kan vi starta rutor via metodanrop??? /JÅ
-	public void showCreateCustomer(){
-		
-		
-	}
-
 	
 	/**
 	 * Sets the overall look and field for the system.
 	 */
 	private void setSystemLookAndFeel() {
 
+		
+		
 		try {
 
 			for (LookAndFeelInfo lnfi : UIManager.getInstalledLookAndFeels()) {
@@ -89,49 +86,70 @@ public class ClientController {
 
 	}
 	
-	
 	/**
-	 * The sidebar.
-	 * 
+	 * Customization of JButton that takes it's ActionCommand as a parameter in the constructor.
 	 * @author Robin Overgaard
 	 * @version 1.0
 	 */
-	private class Sidebar extends JPanel implements ActionListener{
+	private class ActionJButton extends JButton {
+		
+		/**
+		 * Constructor.
+		 * @param text the text to display on this button
+		 */
+		public ActionJButton(String text) {
+			super(text);
+		}
+		
+		/**
+		 * Constructor.
+		 * @param text the text to display on this button
+		 * @param action the action command for this button
+		 */
+		public ActionJButton(String text, String action) {
+			this(text);
+			this.setActionCommand(action);
+		}
+		
+	}
+	
+	/**
+	 * The sidebar.
+	 * @author Robin Overgaard
+	 * @version 1.0
+	 */
+	private class Sidebar extends JPanel implements ActionListener {
 		
 		private JPanel pnlSideNorth = new JPanel();
 		private JPanel pnlSideSouth = new JPanel();
 		
-		/////////////////////////////////////////////////////////////////
-		///////////////////////////EXAMPLES//////////////////////////////
-		/////////////////////////////////////////////////////////////////
-		
 		private JComponent exampleTools[] = new JComponent[] { 
-				new JButton("Customers"), 
-				new JButton("Products"),
-				new JButton("Invoice"), 
-				new JButton("Transactions") };
+				new ActionJButton("Customers"), 
+				new ActionJButton("Products"),
+				new ActionJButton("Invoice"), 
+				new ActionJButton("Transactions") };
 		
 		private JComponent exampleTools2[] = new JComponent[] { 
-				new JButton("Products"),
-				new JButton("Adjust"), 
-				new JButton("Stocktake") };
+				new ActionJButton("Products"),
+				new ActionJButton("Adjust"), 
+				new ActionJButton("Stocktake") };
 		
 		private JComponent exampleTools3[] = new JComponent[] { 
-				new JButton("Users"),
-				new JButton("Database"),
-				new JButton("Settings") };
+				new ActionJButton("Users"),
+				new ActionJButton("Database"),
+				new ActionJButton("Settings") };
 
 		private JComponent exampleInfo[] = new JComponent[] { 
+			
 				new JLabel(Main.APP_NAME, JLabel.CENTER), 
 				new JLabel(Main.APP_VERSION, JLabel.CENTER) };
 		
-		private JComponent exampleShortcuts[] = new JComponent[] { 
-				new JButton("Log out"), 
-				new JButton("Shutdown") };
+//				new JLabel(new ImageIcon (new ImageIcon(ClientController.class.getResource(Main.APP_ICON)).getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH))),
 		
-		/////////////////////////////////////////////////////////////////
-		/////////////////////////////////////////////////////////////////
-		/////////////////////////////////////////////////////////////////
+		private JComponent exampleShortcuts[] = new JComponent[] { 
+				
+				new ActionJButton("Log out"), 
+				new ActionJButton("Shutdown") };
 		
 		public Sidebar() {
 			
@@ -163,7 +181,6 @@ public class ClientController {
 				
 				if (b instanceof JButton) {
 					((JButton) b).addActionListener(this);
-					((JButton) b).setActionCommand(((JButton) b).getText());
 				}
 			}
 			
