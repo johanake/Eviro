5910dad240351c4a25bb4f8db54468446156f5bc
Robin Overgaard <git@robinovergaard.com>
2017-04-26 17:53:39 +0200

    Updated guicontroller

--- src/gui/GUIController.java
+++ src/gui/GUIController.java
@@ -1,4 +1,5 @@
 package gui;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
@@ -24,11 +25,11 @@ import javax.swing.border.TitledBorder;
 
 import client.ClientController;
 import client.Main;
+import tools.CreateCustomer;
 import tools.InvoiceGUI;
 import tools.SearchArticle;
 import tools.SearchCustomer;
 import tools.SearchResults;
-import tools.CreateCustomer;
 
 /**
  * Handles client side gui operations of the system.
@@ -37,7 +38,7 @@ import tools.CreateCustomer;
  */
 
 public class GUIController {
-	
+
 	private JDesktopPane desktop;
 	private ClientController clientController;
 
@@ -46,16 +47,17 @@ public class GUIController {
 	 * @param clientController controller for communication with client
 	 */
 	public GUIController(ClientController clientController) {
-		
+
 		this.clientController = clientController;
-		
+
 		SwingUtilities.invokeLater(new Runnable() {
+			@Override
 			public void run() {
 				setSystemLookAndFeel();
 				JFrame window = new JFrame(Main.APP_NAME + " " + Main.APP_VERSION);
 				JPanel pnlMain = new JPanel(new BorderLayout());
 				desktop = new JDesktopPane();
-				
+
 				window.setContentPane(pnlMain);
 				window.add(desktop, BorderLayout.CENTER);
 				window.add(new Sidebar(), BorderLayout.WEST);
@@ -63,13 +65,20 @@ public class GUIController {
 				window.setMinimumSize(new Dimension(800, 600));
 				window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				window.setVisible(true);
-				window.setJMenuBar(new Menu()); 
+				window.setJMenuBar(new Menu());
 				window.setIconImage(new ImageIcon(Main.APP_ICON).getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH));
 			}
 		});
 
 	}
-	
+
+	/**
+	 * Returns an instance of the GUIController
+	 */
+	public GUIController getGUIController() {
+		return this;
+	}
+
 	/**
 	 * Sets the overall look and field for the system.
 	 */
@@ -99,73 +108,73 @@ public class GUIController {
 	}
 
 	/**
+	 * Adds a new toolbox to the desktop
+	 * @param tool the tool to use
+	 */
+	public void popup(Tool tool) {
+		desktop.add(new Toolbox(tool));
+	}
+
+	/**
 	 * The sidebar.
 	 * @author Robin Overgaard
 	 * @version 1.0
 	 */
 	private class Sidebar extends JPanel implements ActionListener {
-		
+
 		private ArrayList<Toolbox> activeTools = new ArrayList<Toolbox>();
 		private JPanel pnlSideNorth = new JPanel();
 		private JPanel pnlSideSouth = new JPanel();
-		
-		private JComponent quick[] = new JComponent[] { 
-				new ActionJButton("Customer", "find_cust"), 
-				new ActionJButton("Invoice", "find_inv"),
-				new ActionJButton("Transaction", "find_trans"), 
-				new ActionJButton("Article", "find_art") };
 
-		private JComponent tools[] = new JComponent[] { 
-				new ActionJButton("Customer", "tool_customer"), 
-				new ActionJButton("Invoice", "tool_invoice") };
+		private JComponent quick[] = new JComponent[] { new ActionJButton("Customer", "find_cust"), new ActionJButton("Invoice", "find_inv"), new ActionJButton("Transaction", "find_trans"), new ActionJButton("Article", "find_art") };
 
-		private JComponent exampleInfo[] = new JComponent[] { 
-			
-				new JLabel(Main.APP_NAME, JLabel.CENTER), 
-				new JLabel(Main.APP_VERSION, JLabel.CENTER) };
-		
-//				new JLabel(new ImageIcon (new ImageIcon(ClientController.class.getResource(Main.APP_ICON)).getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH))),
-		
-		private JComponent exampleShortcuts[] = new JComponent[] { 
-				
-				new ActionJButton("Settings", "tool_settings"), 
-				new ActionJButton("Quit", "link_exit") };
-		
+		private JComponent tools[] = new JComponent[] { new ActionJButton("Customer", "tool_customer"), new ActionJButton("Invoice", "tool_invoice") };
+
+		private JComponent exampleInfo[] = new JComponent[] {
+
+				new JLabel(Main.APP_NAME, JLabel.CENTER), new JLabel(Main.APP_VERSION, JLabel.CENTER) };
+
+		// new JLabel(new ImageIcon (new ImageIcon(ClientController.class.getResource(Main.APP_ICON)).getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH))),
+
+		private JComponent exampleShortcuts[] = new JComponent[] {
+
+				new ActionJButton("Settings", "tool_settings"), new ActionJButton("Quit", "link_exit") };
+
 		public Sidebar() {
-		
-			setPreferredSize(new Dimension(175,0));
+
+			setPreferredSize(new Dimension(175, 0));
 			setLayout(new BorderLayout());
-			setBorder(new EmptyBorder(10,10,10,10));
-			
+			setBorder(new EmptyBorder(10, 10, 10, 10));
+
 			pnlSideNorth.setLayout(new BoxLayout(pnlSideNorth, BoxLayout.Y_AXIS));
 			pnlSideSouth.setLayout(new BoxLayout(pnlSideSouth, BoxLayout.Y_AXIS));
-			
+
 			pnlSideNorth.add(createComponentPanel(tools, "Tools"));
 			pnlSideNorth.add(createComponentPanel(quick, "Find"));
-			
-//			pnlSideSouth.add(createComponentPanel(exampleInfo, "Information"));
+
+			// pnlSideSouth.add(createComponentPanel(exampleInfo, "Information"));
 			pnlSideSouth.add(createComponentPanel(exampleShortcuts, "System"));
-			
+
 			add(pnlSideNorth, BorderLayout.NORTH);
 			add(pnlSideSouth, BorderLayout.SOUTH);
 		}
-		
+
 		private JPanel createComponentPanel(JComponent[] objects, String title) {
-			
+
 			JPanel pnl = new JPanel();
-			
+
 			pnl.setLayout(new GridLayout(objects.length, 1));
 			pnl.setBorder(new TitledBorder(title));
-			
+
 			for (JComponent b : objects) {
 				pnl.add(b);
-				
+
 				if (b instanceof JButton) {
 					((JButton) b).addActionListener(this);
 				}
-				
+
 			}
-			
+
 			return pnl;
 		}
 
@@ -185,34 +194,33 @@ public class GUIController {
 			case "link_exit":
 				System.exit(0);
 				break;
-				
+
 			case "find_cust":
-				desktop.add(new Toolbox(new SearchCustomer(clientController)));
+				desktop.add(new Toolbox(new SearchCustomer(clientController, getGUIController())));
 				break;
-				
+
 			case "find_trans":
 				System.out.println("hej");
 				desktop.add(new Toolbox(new SearchResults()));
 				break;
-			
+
 			case "find_art":
 				desktop.add(new Toolbox(new SearchArticle(clientController)));
 				break;
-				
 
 			default:
 				JOptionPane.showMessageDialog(desktop, "You clicked on: " + e.getActionCommand());
 				break;
 			}
 		}
-		
+
 		/**
 		 * Customization of JButton that takes it's ActionCommand as a parameter in the constructor.
 		 * @author Robin Overgaard
 		 * @version 1.0
 		 */
 		private class ActionJButton extends JButton {
-			
+
 			/**
 			 * Constructor.
 			 * @param text the text to display on this button
@@ -220,7 +228,7 @@ public class GUIController {
 			public ActionJButton(String text) {
 				super(text);
 			}
-			
+
 			/**
 			 * Constructor.
 			 * @param text the text to display on this button
@@ -230,7 +238,7 @@ public class GUIController {
 				this(text);
 				this.setActionCommand(action);
 			}
-			
+
 		}
 	}
 }
