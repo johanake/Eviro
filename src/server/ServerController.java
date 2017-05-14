4747802e4b36a98cb56bf3a17380f5de83dab3a1
Peter Sjögren <peter.sjogren@live.se>
2017-05-14 18:22:25 +0200

    fix

--- src/server/ServerController.java
+++ src/server/ServerController.java
@@ -312,6 +312,11 @@ public class ServerController {
 							rs.getString(3),
 							rs.getString(4) }));
 
+				} else if (rs.getMetaData().toString().contains("tableName=user")) {
+					ei.add(new ForumMessage(new Object[] {
+							rs.getString(1),
+							rs.getString(2),
+							rs.getString(3), }));
 				}
 			}
 		} catch (SQLException e) {
